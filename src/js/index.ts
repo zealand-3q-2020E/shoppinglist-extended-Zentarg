7d4ba5b991ff99387dbd51145dd0ff313df720dd
zentarg <apg.chili@gmail.com>
2020-09-29 12:11:57 +0200

    Task 1-7

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,24 @@
 //Write your Typescript code here
+let healthy : Array<string> = ["Cashew nuts", "Tomato", "Salad", "Cabbage"];
+
+document.getElementById("newItemAdd").addEventListener('click', () => {
+    let name : string = (<HTMLInputElement> document.getElementById("newItemInput")).value;
+    addNewItemToShoppingList(name);
+});
+
+
+function addNewItemToShoppingList(itemName : string) {
+    let list : HTMLUListElement = <HTMLUListElement> document.getElementById("list");
+    let newItem : HTMLLIElement = document.createElement("li");
+    newItem.setAttribute("id", `${list.childElementCount}`);
+    newItem.setAttribute("class", "unhealthy");
+    for (let index = 0; index < healthy.length; index++) {
+        if (itemName == healthy[index]) {
+            newItem.setAttribute("class", "healthy");
+            break;
+        }
+    }
+
+    newItem.innerText = itemName;
+    list.appendChild(newItem);
+}
